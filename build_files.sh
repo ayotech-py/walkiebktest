78b9adde471010176c53658be725a365c61c3889
ayotech-py <ajaomalik2018@gmail.com>
2024-07-01 14:53:41 +0100

    updated

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,12 @@ pip install -r requirements.txt
 # Download the ffmpeg precompiled binary using curl
 curl -L -o ffmpeg-release-64bit-static.zip https://github.com/GyanD/codexffmpeg/releases/download/5.0/ffmpeg-5.0-essentials_build.zip
 unzip ffmpeg-release-64bit-static.zip
+
+# List the contents of the extracted directory
+echo "Listing contents of extracted directory:"
+ls -R ffmpeg-5.0-essentials_build
+
+# Navigate to the correct directory
 cd ffmpeg-5.0-essentials_build/bin
 
 # Move the ffmpeg binaries to /usr/local/bin
