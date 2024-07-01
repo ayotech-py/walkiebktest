0b06988556175b476af0ed4698166ec2c38c8e74
ayotech-py <ajaomalik2018@gmail.com>
2024-07-01 14:49:38 +0100

    updated

--- build_files.sh
+++ build_files.sh
@@ -4,13 +4,16 @@
 pip install -r requirements.txt
 
 # Download the ffmpeg precompiled binary using curl
-curl -L -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz
-tar -xf ffmpeg.tar.xz
-cd ffmpeg-*-static
+curl -L -o ffmpeg-release-64bit-static.zip https://github.com/GyanD/codexffmpeg/releases/download/5.0/ffmpeg-5.0-essentials_build.zip
+unzip ffmpeg-release-64bit-static.zip
+cd ffmpeg-5.0-essentials_build/bin
 
 # Move the ffmpeg binaries to /usr/local/bin
 mv ffmpeg /usr/local/bin/
 mv ffprobe /usr/local/bin/
 
 # Return to project root directory
-cd ..
+cd ../..
+
+# Collect static files
+python manage.py collectstatic --noinput
