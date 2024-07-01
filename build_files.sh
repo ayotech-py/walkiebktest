d4508ac65b91aff351ea10a8ad44532202e5a70c
ayotech-py <ajaomalik2018@gmail.com>
2024-07-01 17:18:17 +0100

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Install Python dependencies
-#mv ffmpeg /usr/local/bin/ffmpeg
-#mv ffprobe /usr/local/bin/ffprobe
+mv ffmpeg /var/task/ffmpeg
+mv ffprobe /var/task/ffprobe
 
 ls -l /usr/local/bin/ffmpeg
 ls -l /usr/local/bin/
 
 ls /var/task
-
-rm /usr/local/bin/ffmpeg
-rm /usr/local/bin/ffprobe
 
 # Verify that ffmpeg and ffprobe are installed correctly
 
