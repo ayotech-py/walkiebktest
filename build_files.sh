5db344889563ce143237bbd55968e7b45bb8cbd1
ayotech-py <ajaomalik2018@gmail.com>
2024-07-02 02:42:23 +0100

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Install Python dependencies
 pip install -r requirements.txt
-
-# Install FFmpeg using ffmpeg-downloader
-ffmpeg_downloader install --add-path
-
-# Verify the installation
-ffmpeg -version
-ffprobe -version
