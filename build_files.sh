fd72056eb3e585d190e62da09abd36d6b6c39b04
ayotech-py <ajaomalik2018@gmail.com>
2024-07-02 01:51:44 +0100

    uploadd

--- build_files.sh
+++ build_files.sh
@@ -4,26 +4,8 @@
 pip install -r requirements.txt
 
 # Install FFmpeg using ffmpeg-downloader
-python - <<END
-import os
-from ffmpeg_downloader import ffdl
-
-# Install FFmpeg
-ffdl.install(overwrite=True)
-
-# Get the installed path of ffmpeg
-ffmpeg_path = ffdl.ffmpeg_path()
-ffprobe_path = ffdl.ffprobe_path()
-
-# Print paths to use them in the script
-print(f"FFMPEG_PATH={ffmpeg_path}")
-print(f"FFPROBE_PATH={ffprobe_path}")
-END
-
-# Capture the output of the Python script to set environment variables
-FFMPEG_PATH=$(python -c 'from ffmpeg_downloader import ffdl; print(ffdl.ffmpeg_path())')
-FFPROBE_PATH=$(python -c 'from ffmpeg_downloader import ffdl; print(ffdl.ffprobe_path())')
+python -m ffmpeg_downloader install --add-path
 
 # Verify the installation
-$FFMPEG_PATH -version
-$FFPROBE_PATH -version
+ffmpeg -version
+ffprobe -version
