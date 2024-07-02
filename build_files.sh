d2dc96538ba2c34515e56b8a25835e0640e95948
ayotech-py <ajaomalik2018@gmail.com>
2024-07-02 01:36:36 +0100

    uploadd

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Verify that ffmpeg and ffprobe are installed correctly
-
+# Install Python dependencies
 pip install -r requirements.txt
 
-ffdl install --add-path -y
+# Define FFmpeg version and URL
+FFMPEG_VERSION="4.3.2"
+OS_TYPE="linux-64"
+FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-${OS_TYPE}-static.tar.xz"
 
+# Create a directory for FFmpeg binaries
+mkdir -p ffmpeg
+cd ffmpeg
 
+# Download and extract FFmpeg
+wget $FFMPEG_URL -O ffmpeg.tar.xz
+tar -xvf ffmpeg.tar.xz --strip-components=1
 
+# Make FFmpeg and FFprobe available in the PATH
+export PATH=$PATH:$(pwd)
+
+# Verify the installation
+ffmpeg -version
+ffprobe -version
