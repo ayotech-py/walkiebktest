49eb19e59340cc4755c986a1f4fc881982e4bb78
ayotech-py <ajaomalik2018@gmail.com>
2024-07-02 01:42:45 +0100

    uploadd

--- build_files.sh
+++ build_files.sh
@@ -12,9 +12,12 @@ FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-${OS_TYPE}-
 mkdir -p ffmpeg
 cd ffmpeg
 
-# Download and extract FFmpeg using curl
+# Download and extract FFmpeg using curl and unxz
 curl -L $FFMPEG_URL -o ffmpeg.tar.xz
-tar -xvf ffmpeg.tar.xz --strip-components=1
+# Install xz-utils to handle .xz files
+apk add xz
+unxz ffmpeg.tar.xz
+tar -xvf ffmpeg.tar --strip-components=1
 
 # Make FFmpeg and FFprobe available in the PATH
 export PATH=$PATH:$(pwd)
